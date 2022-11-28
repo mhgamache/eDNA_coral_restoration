9a7f8bf89fd440121017d3505069055078680a75
Matthew Gamache <70723207+mhgamache@users.noreply.github.com>
2022-11-28 14:34:24 -0500

    Update and rename 01a_retrieving_basespace.sh to 01a_downloading_basespace.sh

--- 01_retrieving_sequences_and_qc/01a_downloading_basespace.sh
+++ 01_retrieving_sequences_and_qc/01a_downloading_basespace.sh
@@ -1,12 +1,13 @@
-## two positional arguments specifying 1) an run ID and 2) the output directory
+## three positional arguments specifying 1) a run ID; 2) the output directory; 3) email address to alert of job starts, failures, and completions
 ## run IDs can be found via 'bs list run' after loading loadhub.apps/basespace/april.2021
 
 runid=$1
 outdir=$2
+email=$3
 
 mkdir -p ${outdir}
 
-cat <<EOF > ${outdir}/download_basespace.sbatch
+cat <<EOF > ${outdir}/downlaod_basespace.sbatch
 #!/bin/bash
 #SBATCH --qos=rra
 #SBATCH --partition=rra
@@ -14,10 +15,16 @@ cat <<EOF > ${outdir}/download_basespace.sbatch
 #SBATCH --mem=60G
 #SBATCH --job-name=download_basespace
 #SBATCH --output=${outdir}/download_basespace.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=${email}
+
 module purge
 module load hub.apps/basespace/april.2021
+
 bs --version
+
 bs download run -i ${runid} -o ${outdir}
+
 EOF
 
 if $autorun; then
